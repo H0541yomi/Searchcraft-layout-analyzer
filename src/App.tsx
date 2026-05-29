cfd04b871d8d43088e8683486d4db178305d40ad
Ethan Cabalona <cabalona.e@northeastern.edu>
2026-05-29 18:03:45 -0400

    chore: scaffold vite+react+ts project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>SearchCraft Keybind Planner</h1>
+      <p className="subtitle">Optimize your left-hand keyboard layout for searchcrafting</p>
+    </div>
+  )
+}
+
+export default App
